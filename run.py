a151f16b60e4f01dab6a87138e574cdfd06f8fc9
LuciMoore <lucille.a.mo@gmail.com>
2024-02-07 18:44:39 -0600

    specified full paths to images for functions

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from nipype.interfaces import fsl
 from utils import create_crude_LR_mask
 from utils import correct_chirality
 
-root_dir='/home/elisonj/shared/BCP/process/dcan_bibsnet/output_brainmasks'
+root_dir='/home/elisonj/shared/BCP/process/dcan_bibsnet/output_brainmasks/test'
 fsl_lut='/home/elisonj/shared/BCP/process/dcan_bibsnet/code/iterative-chirality-correction/FreeSurferColorLUT.txt'
 os.chdir(root_dir)
 
@@ -14,25 +14,26 @@ subses=glob.glob('sub-*/ses-*')
 subses.sort()
 
 for i in subses:
-    os.chdir(os.path.join(root_dir, i))
     sub=i.split('/')[0]
     ses=i.split('/')[1]
     sub_ses=sub+'_'+ses
     
-    print('Performing iterative chirality correction on {i}')
+    print(f'Performing iterative chirality correction on {i}')
+    
+    sub_root=f'{root_dir}/{i}'
 
     # Make work dir for anat_dilated2 to save intermediate output files in case it's helpful
-    anat_dilated2_wd='anat_dilated2_work'
+    anat_dilated2_wd=f'{sub_root}/anat_dilated2_work'
     if not os.path.exists(anat_dilated2_wd):
         os.mkdir(anat_dilated2_wd)
 
     # Make anat_dilated2 derivatives dir to save final corrected T1w-space aseg out to  
-    anat_dilated2_deriv='anat_dilated2'
+    anat_dilated2_deriv=f'{sub_root}/anat_dilated2'
     if not os.path.exists(anat_dilated2_deriv):
         os.mkdir(anat_dilated2_deriv) 
 
     # Split the chirality corrected acpc aseg image from postbibsnet into left and right to create a crude chirality correction mask 
-    cc_aseg_postBN=f'work/postbibsnet/{i}/chirality_correction/corrected_{sub_ses}_optimal_resized.nii.gz'
+    cc_aseg_postBN=f'{sub_root}/work/postbibsnet/{i}/chirality_correction/corrected_{sub_ses}_optimal_resized.nii.gz'
     crude_mask=f'{anat_dilated2_wd}/crude_mask.nii.gz'
     create_crude_LR_mask(cc_aseg_postBN, crude_mask)
 
@@ -41,13 +42,13 @@ for i in subses:
     correct_chirality(cc_aseg_postBN, crude_mask, crude_corrected_aseg, fsl_lut)
 
     # Iteration 2: Perform chirality correction on the crudely corrected aseg using the original dilated LR mask
-    orig_dil_LRmask=f'work/postbibsnet/{i}/lrmask_dil_wd/LRmask_dil.nii.gz'
+    orig_dil_LRmask=f'{sub_root}/work/postbibsnet/{i}/lrmask_dil_wd/LRmask_dil.nii.gz'
     cc_aseg_iteration2=f'{anat_dilated2_wd}/cc_aseg_acpc.nii.gz'
     correct_chirality(crude_corrected_aseg, orig_dil_LRmask, cc_aseg_iteration2, fsl_lut)
 
     # Transform final corrected aseg into native T1w space 
-    ref_img=f'work/prebibsnet/{i}/averaged/{sub_ses}_0000.nii.gz'
-    init=f'work/postbibsnet/{i}/chirality_correction/seg_reg_to_T1w_native.mat'
+    ref_img=f'{sub_root}/work/prebibsnet/{i}/averaged/{sub_ses}_0000.nii.gz'
+    init=f'{sub_root}/work/postbibsnet/{i}/chirality_correction/seg_reg_to_T1w_native.mat'
     final_deriv_aseg=f'{anat_dilated2_deriv}/{sub_ses}_space-T1w_desc-aseg_dseg.nii.gz'
 
 
